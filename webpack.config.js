eed5a55afe835b5bdd4ecd133c11603368fd261a
dmitriy.drabkin <dmitriy.drabkin@shtormtech.ru>
2021-05-13 14:01:32 +0300

    add extension template

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  mode: 'production',
+  entry: {
+    popup: './src/popup.jsx',
+    background: './src/background.js',
+    content: './src/content.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ["@babel/plugin-transform-react-jsx", { "pragma":"h" }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'resolve-url-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
